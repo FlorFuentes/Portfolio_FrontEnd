12ea9b7705b7ae39ec9a2bcafd7e6be65d1be95f
FlorFuentes <flopy_200412@hotmail.com>
2023-03-04 19:16:54 -0300

    Arregle los skill original

--- src/app/Componentes/habilidade-tecnicas/habilidade-tecnicas.component.ts
+++ src/app/Componentes/habilidade-tecnicas/habilidade-tecnicas.component.ts
@@ -9,14 +9,14 @@ import { MiServicioService } from 'src/app/servicios/mi-servicio.service';
 export class HabilidadeTecnicasComponent implements OnInit{
   miPortfolio: any;
   //Defino variable para recorrer el array de Habilidades
- /*  habilidadLista: any; */
+  habilidadLista: any; 
   constructor(private datosPortfolio: MiServicioService) { }
 
   ngOnInit(): void {
     this.datosPortfolio.obtenerDatos().subscribe(data => {
       console.log(data);
       this.miPortfolio = data;
-   
+      this.habilidadLista=data.habilidadesTecnicas
     });
   }
 }
